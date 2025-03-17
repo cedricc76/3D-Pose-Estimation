164e316ead4392882d1885642d434df0c569b3cb
Cedric Anthony Edysa <123380264+cedricc76@users.noreply.github.com>
2025-03-17 16:23:38 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,79 @@
+import pickle
+import string
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+
+model_dict = pickle.load(open('./model.p', 'rb'))
+model = model_dict['model']
+
+camera = cv2.VideoCapture(0)
+
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+labels_dict = {0 : '5', 1 : '4', 2 : '3', 3 : '2', 4 : '1', 5 : '3', 6 : '2', 7 : '1', 8 : '0'}
+
+while True:
+
+    data_aux = []
+    x_ = []
+    y_ = []
+
+    ret, frame = camera.read()
+
+    H, W, _ = frame.shape
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(frame_rgb)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame,  # image to draw
+                hand_landmarks,  # model output
+                mp_hands.HAND_CONNECTIONS,  # hand connections
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+        for hand_landmarks in results.multi_hand_landmarks:
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+
+                x_.append(x)
+                y_.append(y)
+
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                data_aux.append(x - min(x_))
+                data_aux.append(y - min(y_))
+
+        x1 = int(min(x_) * W) - 10
+        y1 = int(min(y_) * H) - 10
+
+        x2 = int(max(x_) * W) - 10
+        y2 = int(max(y_) * H) - 10
+
+        prediction = model.predict([np.asarray(data_aux)])
+
+        predicted_character = labels_dict[int(prediction[0])]
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+                    cv2.LINE_AA)
+
+    cv2.imshow('frame', frame)
+    cv2.waitKey(1)
+
+
+cap.release()
+cv2.destroyAllWindows()
